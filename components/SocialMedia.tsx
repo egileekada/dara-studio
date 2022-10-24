dacd82510f0b7a05e17c474e08c8541854082410
egileekada <egileoniso.ekada@gmail.com>
2022-10-24 23:32:15 +0100

    update on design

---
+++ components/SocialMedia.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SocialMedia() {
+    return (
+        <div className=' w-full bg-black py-3 font-Poppins-Regular text-white text-sm flex items-center justify-center ' >
+            <a href="http://instagram.com/sbdstudios_" className=" flex items-center"  >
+                <img src='/images/Instagram.png' alt='Instagram' />  
+                <p className=" ml-3" >Instagram</p>   
+            </a>
+        </div>
+    )
+}
