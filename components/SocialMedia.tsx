ae2a33f6e5e30144e02d40ddde5d20150d6626ee
egileekada <egileoniso.ekada@gmail.com>
2022-10-25 14:14:01 +0100

    update on design

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function SocialMedia() {
     return (
         <div className=' w-full bg-black py-3 font-Poppins-Regular text-white text-sm flex items-center justify-center ' >
-            <a href="http://instagram.com/sbdstudios_" className=" flex items-center mr-6"  >
-                <img src='/images/Instagram.png' alt='Instagram' />  
-                <p className=" ml-3" >Instagram</p>   
-            </a>
-            <a href="https://web.facebook.com/SBDStudios" className=" flex ml-6 items-center"  > 
-                <img className=" w-3 " src='/images/Vector.png' alt='Instagram' />  
-                <p className=" ml-3" >FaceBook</p>   
-            </a>
+            <Link href="http://instagram.com/sbdstudios_" >
+                <div className=" flex mr-6 items-center">
+                    <img src='/images/Instagram.png' alt='Instagram' />  
+                    <p className=" ml-3" >Instagram</p>   
+                </div>
+            </Link>
+            <Link href="https://web.facebook.com/SBDStudios"   >
+                <div className=" flex ml-6 items-center">
+                    <img className=" w-3 " src='/images/Vector.png' alt='Instagram' />  
+                    <p className=" ml-3" >FaceBook</p>  
+                </div>  
+            </Link>
         </div>
     )
 }
