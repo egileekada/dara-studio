728c3e6af943153c0fdc2c34f8ef6445d265872c
egileekada <egileoniso.ekada@gmail.com>
2022-10-26 23:36:09 +0100

    update on design

--- components/SliderComponent.tsx
+++ components/SliderComponent.tsx
@@ -18,18 +18,18 @@ export default function SliderComponent(props: any) {
     },) 
 
     return (
-        <div className=' w-full h-screen relative  '  >
-            <div className=' absolute z-10 inset-0 lg:bottom-36  ' style={{background: "linear-gradient(180deg, rgba(0, 0, 0, 0.1) 40.1%, #000000 100%)"}}/>
+        <div className=' w-full h-full relative  '  >
+            <div className=' absolute z-10 inset-0  ' style={{background: "linear-gradient(180deg, rgba(0, 0, 0, 0.1) 40.1%, #000000 100%)"}}/>
             {next === 0 && ( 
-                <img src='/slide.png' className=' w-full h-full lg:pb-36  object-cover ' /> 
+                <img src='/slide.png' className=' w-full h-full  object-cover ' /> 
             )}
             {next === 1 && ( 
-                <img src='/slide1.png' className=' w-full h-full lg:pb-36  object-cover ' /> 
+                <img src='/slide1.png' className=' w-full h-full  object-cover ' /> 
             )}
             {next === 2 && ( 
-                <img src='/slide2.png' className=' w-full h-full lg:pb-36  object-cover ' /> 
+                <img src='/slide2.png' className=' w-full h-full  object-cover ' /> 
             )}
-            <div className=' absolute lg:px-0 px-6 lg:pl-20 bottom-48 z-30 text-white ' >
+            <div className=' absolute lg:px-0 px-6 lg:pl-20 bottom-36 z-30 text-white ' >
                 <div className=' flex items-center ' > 
                     <div className=' h-[1.2px] bg-[#C48F56] w-6 ' />
                     <p className=' font-Poppins-Regular text-sm ml-4 ' >Portraits</p>
@@ -45,7 +45,7 @@ export default function SliderComponent(props: any) {
                 )}
                 <p className=' font-Poppins-Regular text-sm hidden lg:flex items-center ' >READ MORE <span className='text-xl ml-1' >{">"}</span></p>
             </div>
-            <div className=' w-full absolute z-30 bottom-40 lg:hidden flex justify-center items-center ' >
+            <div className=' w-full absolute z-30 bottom-28 lg:hidden flex justify-center items-center ' >
                 <p className={next === 0 ? ' text-[#C48F56] mx-2 ' : ' text-white mx-2 ' } >01</p>
                 {next === 0 && (
                     <div className=' h-[1px] bg-white w-6 mx-2 ' />
